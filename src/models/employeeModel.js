7181d6937a5c86598c62d815ff3a0f54206fba3d
Heitor Souza <heitor.sf5@gmail.com>
2023-09-12 19:30:59 -0300

    Added UPDATE employee function

--- src/models/employeeModel.js
+++ src/models/employeeModel.js
@@ -11,7 +11,6 @@ const createEmployee = async (req, res) => {
       'INSERT INTO Employee (name, role) VALUES (?, ?)',
       [name, role]
     );
-    // return true;
     return res.status(201).json({message: 'New employee created successfully!'})
   } catch(err) { return res.status(400).json({message: 'Something went wrong!!!'}) }
 };
@@ -19,9 +18,19 @@ const createEmployee = async (req, res) => {
 const readEmployees = async (_req, res) => {
   try {
     const [employees] = await connection.execute('SELECT * FROM Employee')
-    // return employees
     return res.status(200).json(employees)
   } catch(err) { return res.status(500).json({message: "Server error!"}) }
 };
 
-module.exports = { createEmployee, readEmployees };
+const updateEmployee = async (req, res) => {
+  try {
+    const { name, role, id } = req.body;
+    await connection.execute(
+      'UPDATE Employee SET name=?, role=? WHERE id=?',
+      [name, role, id]
+    )
+    return res.status(200).json({message: 'Employee updated successfully!'})
+  } catch(err) { return res.status(404).json({message: 'Employee not found!'}) }
+}
+
+module.exports = { createEmployee, readEmployees, updateEmployee };
--- src/routes/employeeRouter.js
+++ src/routes/employeeRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { readEmployees, createEmployee } = require('../models/employeeModel');
+const { readEmployees, createEmployee, updateEmployee } = require('../models/employeeModel');
 
 const employeeRouter = Router();
 
 employeeRouter
   .post('/employee', createEmployee)
   .get('/employee', readEmployees)
+  .put('/employee', updateEmployee)
 
 module.exports = employeeRouter;
